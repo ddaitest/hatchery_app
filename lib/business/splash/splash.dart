bbb4ed0ee93e911cf07df415b6f58fc5a2b064fb
seven <lc8681@gmail.com>
2019-11-14 17:13:15 +0800

    修改启动白屏

--- lib/business/splash/splash.dart
+++ lib/business/splash/splash.dart
@@ -8,8 +8,40 @@ import 'package:provider/provider.dart';
 
 import '../../configs.dart';
 
-class SplashPage extends StatelessWidget {
-  static const length = 3;
+class SplashPage extends StatefulWidget {
+  @override
+  SplashState createState() => SplashState();
+}
+
+class SplashState extends State<SplashPage> {
+  Timer _timer;
+
+  //倒计时数值
+  var countdownTime = 1;
+
+  //页面初始化状态的方法
+  @override
+  void initState() {
+    super.initState();
+    //开启倒计时
+    startCountdown();
+  }
+
+  //倒计时方法
+  startCountdown() {
+    countdownTime = SPLASH_TIME;
+    final call = (timer) {
+      setState(() {
+        print("###### $countdownTime");
+        if (countdownTime < 1) {
+          _timer.cancel();
+        } else {
+          countdownTime -= 1;
+        }
+      });
+    };
+    _timer = Timer.periodic(Duration(seconds: 1), call);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,15 +60,36 @@ class SplashPage extends StatelessWidget {
     });
     return Consumer<SplashManager>(
       builder: (context, manager, child) => Container(
-        constraints: BoxConstraints.expand(
-          width: double.infinity,
-          height: double.infinity,
-        ),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: ExactAssetImage(manager.splashUrl),
-            fit: BoxFit.cover,
-          ),
+        constraints: BoxConstraints.expand(),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: Image.asset(
+                manager.splashUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              width: 75.0,
+              top: 50.0,
+              right: 20.0,
+              //控件透明度 0.0完全透明，1.0完全不透明
+              child: Opacity(
+                opacity: 0.8,
+                child: FlatButton(
+                  color: Colors.grey,
+                  colorBrightness: Brightness.dark,
+                  splashColor: Colors.grey,
+                  child: Text("跳过 $countdownTime"),
+                  onPressed: () {
+                    _gotoHomePage(context);
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/configs.dart
+++ lib/configs.dart
@@ -1,5 +1,5 @@
 const bool TEST = true;
 
-const int SPLASH_TIME = TEST ? 1 : 3;
+const int SPLASH_TIME = TEST ? 3 : 3;
 
 const String AMAP_KEY = "30a97518348a9b6b8cc652b2dbabe3a2";
