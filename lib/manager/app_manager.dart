46065f0ff9ac6c47678618e18cf0f118c4d1f8d7
seven <lc8681@gmail.com>
2019-11-20 20:59:11 +0800

    修改周边页

--- lib/manager/app_manager.dart
+++ lib/manager/app_manager.dart
@@ -40,7 +40,7 @@ class AppManager extends ChangeNotifier {
     Response response = await ApiForServicePage.queryServiceList();
     final parsed = json.decode(response.data);
     var resultCode = parsed['code'] ?? 0;
-    var resultData = parsed['result'][0]['content'];
+    var resultData = parsed['result'][1]['content'];
     if (resultCode == 200 && resultData != null) {
       var newData = resultData
           .map<ServiceListInfo>((json) => ServiceListInfo.fromJson(json))
