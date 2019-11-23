88bcf9a36a962993e1f4d3dc799a04886ec13b38
daining <daining@bigo.sg>
2019-11-23 23:23:46 +0800

    修改倒计时

--- lib/business/splash/splash.dart
+++ lib/business/splash/splash.dart
@@ -5,47 +5,12 @@ import 'package:hatchery/business/home/home.dart';
 import 'package:hatchery/manager/splash_manager.dart';
 import 'package:provider/provider.dart';
 
-import '../../configs.dart';
-
 class SplashPage extends StatefulWidget {
   @override
   SplashState createState() => SplashState();
 }
 
 class SplashState extends State<SplashPage> {
-  Timer _timer;
-
-  //倒计时数值
-  var countdownTime = 0;
-
-  //页面初始化状态的方法
-  @override
-  void initState() {
-    super.initState();
-    //开启倒计时
-    startCountdown();
-  }
-
-  void _gotoHomePage(BuildContext context) => Navigator.pushReplacement(
-      context, MaterialPageRoute(builder: (context) => HomePage()));
-
-  //倒计时方法
-  startCountdown() {
-    countdownTime = SPLASH_TIME;
-    final call = (timer) {
-      setState(() {
-        print("LC => countDown###### $countdownTime");
-        if (countdownTime <= 1) {
-          _timer.cancel();
-          _gotoHomePage(context);
-        } else {
-          countdownTime -= 1;
-        }
-      });
-    };
-    _timer = Timer.periodic(Duration(seconds: 1), call);
-  }
-
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
@@ -54,50 +19,45 @@ class SplashState extends State<SplashPage> {
     );
   }
 
-  _splashPage(BuildContext context) {
-    return Consumer<SplashManager>(
-      builder: (context, manager, child) => Container(
-        constraints: BoxConstraints.expand(),
-        child: Stack(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              child: Image.asset(
-                manager.splashUrl,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Positioned(
-              width: 75.0,
-              top: 50.0,
-              right: 20.0,
-              //控件透明度 0.0完全透明，1.0完全不透明
-              child: Opacity(
-                opacity: 0.4,
-                child: FlatButton(
-                  color: Colors.black,
-                  colorBrightness: Brightness.dark,
-                  splashColor: Colors.black,
-                  child: Text("跳过 $countdownTime"),
-                  onPressed: () {
-                    _gotoHomePage(context);
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+  void _gotoHomePage2(BuildContext bc) async {
+    Future.microtask(() => Navigator.pushReplacement(
+        bc, MaterialPageRoute(builder: (bc) => HomePage())));
   }
 
-  //跳转后销毁计时器
-  @override
-  void dispose() {
-    super.dispose();
-    if (_timer != null) {
-      _timer.cancel();
-    }
+  _splashPage(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(),
+      child: Stack(
+        children: <Widget>[
+          Container(
+              width: double.infinity,
+              height: double.infinity,
+              child: Consumer<SplashManager>(
+                  builder: (context, manager, child) =>
+                      Image.asset(manager.splashUrl, fit: BoxFit.cover))),
+          Positioned(
+            width: 75.0,
+            top: 50.0,
+            right: 20.0,
+            //控件透明度 0.0完全透明，1.0完全不透明
+            child: Opacity(
+              opacity: 0.4,
+              child: FlatButton(
+                color: Colors.black,
+                colorBrightness: Brightness.dark,
+                splashColor: Colors.black,
+                child: Consumer<SplashManager>(builder: (ct, manager, c) {
+                  if (manager.countdown<1){
+                    _gotoHomePage2(ct);
+                  }
+                  return Text("跳过 ${manager.countdown}");
+                }),
+                onPressed: () => _gotoHomePage2(context),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/manager/splash_manager.dart
+++ lib/manager/splash_manager.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:hatchery/business/home/home.dart';
 
 class SplashManager extends ChangeNotifier {
   String _splashUrl = 'images/welcome.png';
@@ -8,4 +12,30 @@ class SplashManager extends ChangeNotifier {
   String _splashTip = "泰达希尔\n暴风城\n";
 
   String get splashTip => _splashTip;
+
+  int countdown = 5;
+
+  SplashManager() {
+    _startCountdown();
+  }
+
+  Timer timer;
+
+  /// 初始化
+  _startCountdown() async {
+    final timeUp = (Timer timer) {
+      countdown--;
+      notifyListeners();
+      if (countdown < 0) {
+        timer.cancel();
+      }
+    };
+    timer = Timer.periodic(Duration(seconds: 1), timeUp);
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
 }
