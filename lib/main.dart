03a12862199beab3dece8a93d56796b2669a10b3
lichen <lc8681@gmail.com>
2019-12-15 23:29:24 +0800

    增加Jpush

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:hatchery/flavors/Flavors.dart';
-import 'package:hatchery/manager/app_manager.dart';
-import 'package:provider/provider.dart';
 import 'business/splash/splash.dart';
 import 'business/home/home.dart';
 import 'package:flutter_bugly/flutter_bugly.dart';
