88c36574f78db5aa04d30758978c33081b4597f7
seven <lc8681@gmail.com>
2020-01-03 18:54:02 +0800

    报事增加图片上传功能

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'business/splash/splash.dart';
 import 'business/home/home.dart';
 import 'package:flutter_bugly/flutter_bugly.dart';
 import 'package:flutter_jpush/flutter_jpush.dart';
+import 'package:provider/provider.dart';
+import 'package:hatchery/manager/app_manager.dart';
 
 void main() async {
 //  FlutterBugly.postCatchedException(() {
@@ -34,13 +36,16 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   void didChangeAppLifecycleState(AppLifecycleState state) {
     switch (state) {
       case AppLifecycleState.inactive: // 处于这种状态的应用程序应该假设它们可能在任何时候暂停。
+        print("LC -> ################# inactive");
         break;
       case AppLifecycleState.resumed: // 应用程序可见，前台
+        print("LC -> ################# resumed");
         break;
       case AppLifecycleState.paused: // 应用程序不可见，后台
         print("LC -> ################# paused");
         break;
       case AppLifecycleState.suspending: // 申请将暂时暂停
+        print("LC -> ################# suspending");
         break;
     }
   }
@@ -48,13 +53,16 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: Flavors.strings.title,
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: SplashPage(),
-      routes: {
-        '/home': (context) => HomePage(),
-      },
+    return ChangeNotifierProvider(
+      builder: (context) => AppManager(),
+      child: MaterialApp(
+        title: Flavors.strings.title,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: SplashPage(),
+        routes: {
+          '/home': (context) => HomePage(),
+        },
+      ),
     );
   }
 
