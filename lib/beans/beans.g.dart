d0b1b4f2dc7440705ef23616210bdbb54f6c4187
daining <daining@bigo.sg>
2020-01-20 15:53:49 +0800

    添加 JSON 自动解析的产物

---
+++ lib/beans/beans.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'beans.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Post _$PostFromJson(Map<String, dynamic> json) {
+  return Post(
+    json['id'] as String,
+    json['title'] as String,
+    json['summary'] as String,
+    json['content'] as String,
+    json['status'] as num,
+    json['client_id'] as String,
+    json['update_time'] as num,
+    json['create_time'] as num,
+  );
+}
+
+Map<String, dynamic> _$PostToJson(Post instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'summary': instance.summary,
+      'content': instance.content,
+      'status': instance.status,
+      'client_id': instance.client_id,
+      'update_time': instance.update_time,
+      'create_time': instance.create_time,
+    };
