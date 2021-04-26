1609287979b0457313aa815248cfcb322e33b28b
daining <daining@bigo.sg>
2021-04-27 01:00:50 +0800

    添加 PUSH

---
+++ android/app/src/main/kotlin/com/daivp/hatchery/XApp.kt
@@ -0,0 +1,12 @@
+package com.daivp.hatchery
+
+import android.app.Application
+import cn.jpush.android.api.JPushInterface
+
+class XApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        JPushInterface.setDebugMode(true);
+        JPushInterface.init(this);
+    }
+}
